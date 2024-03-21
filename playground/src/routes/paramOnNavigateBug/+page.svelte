4c7d474d4657f5f2eb034bd7ab4021737fc47fe8
Matthijs Kralt <matthijs@webinargeek.com>
2024-03-21 14:09:11 +0800

    test: Add test for the param navigation bug

---
+++ playground/src/routes/paramOnNavigateBug/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { queryParam, ssp } from 'sveltekit-search-params';
+
+	const param1 = queryParam('param1', ssp.string());
+	const param2 = queryParam('param2', ssp.string());
+
+	param1.subscribe(() => {
+		param2.set('updated param2');
+	});
+</script>
+
+<button
+	data-testid="update-params"
+	on:click={() => ($param1 = 'updated param1')}>Update params</button
+>
