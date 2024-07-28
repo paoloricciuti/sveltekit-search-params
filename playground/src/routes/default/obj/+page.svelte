ae3d890d8984a2c06eceae7841c0f950cf447f48
paoloricciuti <ricciutipaolo@gmail.com>
2024-07-28 12:45:00 +0200

    fix: avoid complex values default being overriden on write

---
+++ playground/src/routes/default/obj/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { queryParam, ssp } from 'sveltekit-search-params';
+
+	const obj = queryParam('obj', ssp.object({ test: '' }));
+</script>
+
+<a href="/default/obj">reset</a>
+
+{#if $obj}
+	<input bind:value={$obj.test} />
+{/if}
